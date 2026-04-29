4d3afd38a4f4993d84515b4efce2acdda0a2ae5c
Jasanpreet02 <jass2008kaur@gmail.com>
2026-04-29 10:12:05 +0530

    Updates

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,3 +1,23 @@
+// import axios from 'axios';
+
+// export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+// const options = {
+//   params: {
+//     maxResults: 50,
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+//   },
+// };
+
+// export const fetchFromAPI = async (url) => {
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+//   return data;
+// };
+
 import axios from 'axios';
 
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
@@ -13,7 +33,10 @@ const options = {
 };
 
 export const fetchFromAPI = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
-  return data;
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+  } catch (error) {
+    console.error("API ERROR:", error.response?.data || error.message);
+  }
 };
