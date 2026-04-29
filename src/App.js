9e26387ab910248c7a4e60c6620a33221098fe63
Jasanpreet02 <jass2008kaur@gmail.com>
2026-04-29 10:31:15 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Box } from '@mui/material';
+
+// import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+// const App = () => (
+//   <BrowserRouter>
+//     <Box sx={{ backgroundColor: '#000' }}>
+//       <Navbar />
+//       <Routes>
+//         <Route exact path='/' element={<Feed />} />
+//         <Route path='/video/:id' element={<VideoDetail />} />
+//         <Route path='/channel/:id' element={<ChannelDetail />} />
+//         <Route path='/search/:searchTerm' element={<SearchFeed />} />
+//       </Routes>
+//     </Box>
+//   </BrowserRouter>
+// );
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
+  >
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Feed />} />
+        {/* ❌ remove 'exact' (not needed in v6) */}
+        <Route path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
