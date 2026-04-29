1da4a7ad36b4340f70270625da93bad707c3bbce
Jasanpreet02 <jass2008kaur@gmail.com>
2026-04-29 10:41:24 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Box } from '@mui/material';
-
-// import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
-
-// const App = () => (
-//   <BrowserRouter>
-//     <Box sx={{ backgroundColor: '#000' }}>
-//       <Navbar />
-//       <Routes>
-//         <Route exact path='/' element={<Feed />} />
-//         <Route path='/video/:id' element={<VideoDetail />} />
-//         <Route path='/channel/:id' element={<ChannelDetail />} />
-//         <Route path='/search/:searchTerm' element={<SearchFeed />} />
-//       </Routes>
-//     </Box>
-//   </BrowserRouter>
-// );
-
-// export default App;
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
@@ -34,7 +13,7 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        {/* ❌ remove 'exact' (not needed in v6) */}
+        {/*  remove 'exact' (not needed in v6) */}
         <Route path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
